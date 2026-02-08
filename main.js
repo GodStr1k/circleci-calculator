28bbed9354ab850ecf20081bad2d99d321e4c7f8
SAHANA2519 <sahana.a2023a@vitstudent.ac.in>
2026-02-09 00:08:43 +0530

    add sub feature

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ let b = Number(document.getElementById("num2").value);
 document.getElementById("result").innerText =
 "Result: " + add(a,b);
 }
+function subtractNumbers(){
+let a = Number(document.getElementById("num1").value);
+let b = Number(document.getElementById("num2").value);
+
+document.getElementById("result").innerText =
+"Result: " + subtract(a,b);
+}
