f078f79a884a3f611ab79b6274287d011374a872
SharonFiona <sharon.fiona2023@vitstudent.ac.in>
2026-02-09 00:32:48 +0530

    added multiply feature

--- main.js
+++ main.js
@@ -12,3 +12,10 @@ let b = Number(document.getElementById("num2").value);
 document.getElementById("result").innerText =
 "Result: " + subtract(a,b);
 }
+function multiplyNumbers(){
+let a = Number(document.getElementById("num1").value);
+let b = Number(document.getElementById("num2").value);
+
+document.getElementById("result").innerText =
+"Result: " + multiply(a,b);
+}
